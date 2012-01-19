6fbc9501efbeed0cc5dc69e2cc90c13a7e9c42f8
whtsky <whtsky@vip.qq.com>
2012-01-19 23:45:21 +0800

    增加提示音&&修复BUG

--- 3gfetion.py
+++ 3gfetion.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from PySide.QtCore import *
 from PySide.QtGui import *
 from PySide.QtWebKit import *
 from PySide.QtNetwork import *
-import config
+from PySide.phonon import Phonon
+import config,sys
 
 bestsize = QSize(337, 623)
 
@@ -34,7 +36,7 @@ class Browser(QWidget):
         
         #设置菜单及行为
         self.restoreAction = QAction(u'显示窗口', self,triggered=self.showNormal)
-        self.quitAction = QAction(u'退出', self,triggered=qApp.quit)
+        self.quitAction = QAction(u'退出', self,triggered=self.quit)
         self.trayIconMenu = QMenu(self)
         self.trayIconMenu.addAction(self.restoreAction)
         self.trayIconMenu.addAction(self.quitAction)
@@ -46,6 +48,9 @@ class Browser(QWidget):
         #通知区域icon显示
         self.iconComboBox.setCurrentIndex(1)
         self.trayIcon.activated.connect(self.iconActivated)
+        
+        #提示音
+        self.notify = Phonon.createPlayer(Phonon.MusicCategory,Phonon.MediaSource(config.notifyfile))
         
     def iconActivated(self, reason):
         if reason in (QSystemTrayIcon.Trigger,QSystemTrayIcon.DoubleClick):
@@ -61,6 +66,8 @@ class Browser(QWidget):
             return None
         unread = int(self.reply.readAll()[15:-1])
         if unread>0 and unread!=self.unread:#有新消息
+            self.notify.stop()
+            self.notify.play()
             self.trayIcon.showMessage(u'提示',u'您有%s条未读飞信信息，请及时回复' % unread, QSystemTrayIcon.MessageIcon(),1000)
             self.unread = unread
         self.checkmessage()
@@ -72,8 +79,9 @@ class Browser(QWidget):
         self.trayIcon.setToolTip(self.iconComboBox.itemText(index))
         
     def closeEvent(self,event):
-        self.hide()
+        self.setVisible(False)
         self.trayIcon.show()
+        self.unread = 0
         self.webView.load(QUrl('http://f.10086.cn/im5/index/html5.action'))#防止遗漏消息
         event.ignore()
         self.checkmessage()
@@ -84,14 +92,16 @@ class Browser(QWidget):
 
     def logindone(self):
         self.webView.load(QUrl('http://f.10086.cn/im5/index/html5.action'))
-        self.show()
+        self.setVisible(True)
         self.unread = 0
         
-    def __del__(self):
+    def quit(self):
+        self.hide()
         request = QNetworkRequest(QUrl('http://f.10086.cn/im/index/logoutsubmit.action'))
         self.reply = self.network_manager.get(request)
+        self.reply.finished.connect(qApp.quit)
 
 if __name__ == '__main__':
-    app = QApplication([])
+    app = QApplication(sys.argv)
     ui = Browser()
-    app.exec_()
+    sys.exit(app.exec_())
